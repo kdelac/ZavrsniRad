1d359689d376376f3c6de08c28d26c4c0bff80ab
Kristijan Delač <34710959+kdelac@users.noreply.github.com>
2020-09-18 22:25:09 +0200

    SignalR

---
+++ ZavrsniRadSignalR/SignalR/Hubs/PositionHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Hubs
+{
+    public class PositionHub : Hub
+    {
+        public async Task PositionFromServer(float x, float y, int height, int width)
+        {
+            Console.WriteLine("Recive position from server x: " +x + "y: " +y);
+            await Clients.All.SendAsync("maca", x, y, height, width);
+        }
+    }
+}

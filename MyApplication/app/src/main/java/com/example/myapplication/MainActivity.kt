508b1b9dbc80f1014485246abcdc9795b96a8b36
Kristijan Delač <34710959+kdelac@users.noreply.github.com>
2020-09-19 22:49:37 +0200

    Dorada

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,11 +10,18 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.nexenio.bleindoorpositioning.ble.beacon.BeaconManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.pow
 
 
 class MainActivity : AppCompatActivity() {
 
     private val signalRSpajanje = SignalRSpajanje.create()
+    val x1 = 0.0
+    val y1 = 0.0
+    val x2 = 3.0
+    val y2 = 0.0
+    val x3 = 5.0
+    val y3 = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +31,7 @@ class MainActivity : AppCompatActivity() {
 
         val beaconScanner = BeaconScanner.Builder(this)
             .setBeaconBatchListener { beacons: Collection<Beacon> ->
-                beacons.forEach{
-                    println(it.getIdentifierAsUuid(1))
-                }
+
             }
             .setRangingEnabled(ArmaFilter.Builder())
             .build()
@@ -36,8 +41,7 @@ class MainActivity : AppCompatActivity() {
 
 
         click.setOnClickListener{
-            setPosition(getX(), getY(), getHeight(), getWidth())
-
+            setPosition(getX().toFloat(), getY().toFloat(), getHeight(), getWidth())
         }
     }
 
@@ -50,20 +54,27 @@ class MainActivity : AppCompatActivity() {
         signalRSpajanje.Poslaji(x, y, height, width)
     }
 
-    private fun processScanResult(scanResult: ScanResult) {
-        val macAddress = scanResult.device.address
-        val advertisingData = scanResult.scanRecord?.bytes
-        val rssi = scanResult.rssi
-        BeaconManager.processAdvertisingData(macAddress, advertisingData, rssi)
+
+    private fun getX(r1: Float, r2: Float, r3: Float) : Double{
+        val a = 2*x2 - 2*x1
+        val b = 2*y2 - 2*y1
+        val c = r1.pow(2) - r2.pow(2) - x1.pow(2) + x2.pow(2) - y1.pow(2) + y2.pow(2)
+        val d = 2*x3 - 2*x2
+        val e = 2*y3 - 2*y2
+        val f = r2.pow(2) - r3.pow(2) - x2.pow(2) + x3.pow(2) - y2.pow(2) + y3.pow(2)
+        val x = (c*e - f*b) / (e*a - b*d)
+        return x;
     }
 
-
-    private fun getX() : Float{
-        return (0..720).random().toFloat()
-    }
-
-    private fun getY() : Float{
-        return (0..1400).random().toFloat()
+    private fun getY(r1: Float, r2: Float, r3: Float) : Float{
+        val a = 2*x2 - 2*x1
+        val b = 2*y2 - 2*y1
+        val c = r1.pow(2) - r2.pow(2) - x1.pow(2) + x2.pow(2) - y1.pow(2) + y2.pow(2)
+        val d = 2*x3 - 2*x2
+        val e = 2*y3 - 2*y2
+        val f = r2.pow(2) - r3.pow(2) - x2.pow(2) + x3.pow(2) - y2.pow(2) + y3.pow(2)
+        val y = (c*d - a*f) / (b*d - a*e)
+        return y;
     }
 
     private fun getWidth() : Float{
@@ -75,6 +86,9 @@ class MainActivity : AppCompatActivity() {
     private fun getHeight(): Float{
         return third_beacon.y
     }
+
+
+    private fun ge
 
 
 
